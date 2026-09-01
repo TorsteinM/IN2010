91975de21486b09e449a92e43b0c89e8990d6d61
Torstein Ekroll <torsteinem@gmail.com>
2026-09-01 20:58:12 +0200

    Added proper constructors

--- Oblig1/src/Deque.java
+++ Oblig1/src/Deque.java
@@ -2,15 +2,27 @@ public class Deque {
     // Implementerer en double ended queue rundt en sirkulær buffer
     private CircularBuffer buffer;
 
+    public Deque() {
+        buffer = new CircularBuffer(16);
+    }
+
+    public Deque(int initCapacity) {
+        buffer = new CircularBuffer(initCapacity);
+    }
+    
     public Deque(int[] array) {
         buffer = new CircularBuffer(array);
     }
-    
+
     public void push_back(int value){
         buffer.push_back(value);
     }
 
     public void push_front(int value){
         buffer.push_front(value);
+    }
+
+    public int get(int i){
+        return buffer.get(i);
     }
 }
