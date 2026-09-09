71f083fb95822798a3ab4dfb48ee95db148a8c2b
Torstein Ekroll <torsteinem@gmail.com>
2026-09-10 00:45:36 +0200

    TODO: Implementer Merge Sort

---
+++ Oblig1/src/MergeSort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class MergeSort {
+    static public int[] sort(int[] array) {
+        int[] sorted = new int[array.length];
+
+        return sorted;
+    }
+
+
+    public static void main (String[] args) {
+        System.out.println(Arrays.toString(sort(new int[]{1,2,3})));
+    }
+}
